566ed097a911d1fb4a0cdfdc7e271b3b0c0770ca
jamolpe <jamolpeeps@gmail.com>
2017-03-11 11:51:23 +0100

    Cambios servicios proveedores + BD

---
+++ practica2/proveedor1.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-03-2017 a las 11:49:09
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proveedor1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `piezas`
+--
+
+CREATE TABLE `piezas` (
+  `id` int(11) NOT NULL,
+  `Precio` float NOT NULL,
+  `Referencia` varchar(11) NOT NULL,
+  `Unidades` int(11) NOT NULL,
+  `Factura` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `piezas`
+--
+
+INSERT INTO `piezas` (`id`, `Precio`, `Referencia`, `Unidades`, `Factura`) VALUES
+(1, 10, 'aaa1', 9995, 12.5),
+(2, 2.5, 'aaa2', 10000, 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `piezas`
+--
+ALTER TABLE `piezas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `piezas`
+--
+ALTER TABLE `piezas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
